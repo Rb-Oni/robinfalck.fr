9aba78b812beda9b4f38838dc75ca71dcdd08d6f
Robin FALCK <robin.falck57@gmail.com>
2022-02-21 12:08:21 +0100

    contact and about responsive

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-<div class="container mx-auto mb-5 w-1/3 h-screen">
-    <h1 class="text-center my-16 font-bold text-white text-8xl break-words">À PROPOS</h1>
+<div class="container mx-auto mb-5 w-screen md:w-1/3">
+    <h1 class="text-center my-16 font-bold text-white text-6xl lg:text-8xl">À PROPOS</h1>
     <div class="flex flex-col text-white font-semibold">
-        <p class="mb-10 text-lg text-center">Robin, 24 ans, Développeur web junior. J'essaye de coder des sites web quand je code pas des sites web, en ce moment j'apprends Laravel.</p>
+        <p class="mb-10 text-md lg:text-lg text-center">Robin, 24 ans, Développeur web junior. J'essaye de coder des sites web quand je code pas des sites web, en ce moment j'apprends Laravel.</p>
         <ul class="text-center mb-5">
-            <li class="font-bold text-2xl text-white">FORMATIONS : </li>
-            <li class="my-5 text-lg"><span class="font-bold">BTS Systèmes Numériques</span><br> option Électronique et Communications</li>
-            <li class="text-lg"><span class="font-bold">BAC PRO Systèmes Électroniques Numériques</span><br> option Télécommunications et Réseaux</li>
+            <li class="font-bold text-lg lg:text-2xl text-white">FORMATIONS : </li>
+            <li class="my-5 text-md lg:text-lg"><span class="font-bold">BTS Systèmes Numériques</span><br> option Électronique et Communications</li>
+            <li class="text-md lg:text-lg"><span class="font-bold">BAC PRO Systèmes Électroniques Numériques</span><br> option Télécommunications et Réseaux</li>
         </ul>
     </div>
 </div>
